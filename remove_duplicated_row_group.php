7be9ce8d1cf7a48e617b2b5d40d4c11812b34e3e
billiejoenahema <billiejoenahema@gmail.com>
2023-12-20 08:31:02 +0900

    update: remove_duplicated_row_group.php

--- remove_duplicated_row_group.php
+++ remove_duplicated_row_group.php
@@ -1,23 +1,55 @@
 <?php
 
-// 元のCSVファイルのパス
+// 処理前にバックアップファイルファイルをリネームしておく
+// concatenated.csv
+// ↓
+// concatenated_backup.csv
+
+// 元ファイルのパス
+// $originalFilePath = '/directory/' . $argv[1] .  '/concatenated_backup.csv';
+// 処理後ファイルのパス
+// $newFilePath = '/directory/' . $argv[1] .  '/concatenated.csv';
 $originalFilePath = './concatenated.csv';
+$newFilePath = './concatenated_after.csv';
 
-// 新しいCSVファイルのパス
-$newFilePath = 'concatenated_' . date('YmdHis') . '.csv';
+// 元ファイルの存在チェック
+if (!file_exists($originalFilePath)) {
+    die("Error: ファイルが見つかりません。\n" . $originalFilePath . "\n");
+}
 
-// 元のCSVファイルを読み込む
+// 元ファイルを読み込む
 $originalCsv = array_map('str_getcsv', file($originalFilePath));
+
+// 処理後のファイルがすでに存在する場合はファイルを削除する
+if (file_exists($newFilePath)) {
+    unlink($newFilePath);
+}
+
+// 元ファイルがCSVかどうかをチェック
+if (!is_array($originalCsv)) {
+    die("Error: ファイルがCSV形式ではありません。\n");
+}
+// 元ファイルが空であるかをチェック
+if (empty($originalCsv)) {
+    die("Error: ファイルが空です。\n");
+}
+
+// 元ファイルの行数
+$originalRowCount = count($originalCsv);
+
+// 1列目の値が空の行がある場合の処理
+
 
 // グループごとの配列を初期化
 $groups = array();
 
+// 1列目に同じ値が連続している行のかたまりを1グループと定義する
 // グループごとに分割
 $groupId = null;
 foreach ($originalCsv as $row) {
     $currentGroupId = $row[0];
 
-    // グループが変わったら新しいグループとして扱う
+    // 1列目の値の連続性が途切れたら新しいグループとして扱う
     if ($currentGroupId !== $groupId) {
         $groups[] = array();
         $groupId = $currentGroupId;
@@ -27,33 +59,40 @@ foreach ($originalCsv as $row) {
     $groups[count($groups) - 1][] = $row;
 }
 
-// 既に新しいCSVに存在する1列目の値を保持する配列
-$existingIds = array();
+// すでに追加したグループに存在する1列目の値を保持する配列
+$existingIds = [];
+
+// 新しいCSVファイルに追加しなかったグループの1行目の1列目の値を保持する配列
+$removedIds = [];
 
 // 新しいCSVファイルに書き込む
+$fp = fopen($newFilePath, 'a');
+
 foreach ($groups as $group) {
     $groupId = $group[0][0];
 
     // 新しいCSVに追加する前に、既に追加したグループの1行目の1列目に同じ値があるか確認
-    $skipGroup = false;
-    foreach ($existingIds as $existingId) {
-        if ($existingId === $groupId) {
-            $skipGroup = true;
-            break;
-        }
-    }
+    $skipGroup = in_array($groupId, $existingIds);
 
     // 同じ1列目の値が含まれていない場合、新しいCSVに追加
     if (!$skipGroup) {
-        $fp = fopen($newFilePath, 'a');
         foreach ($group as $row) {
             fputcsv($fp, $row);
         }
-        fclose($fp);
 
         // 既に新しいCSVに存在する1列目の値を更新
         $existingIds[] = $groupId;
+    } else {
+        $removedIds[] = $groupId;
     }
 }
 
-echo "新しいCSVが作成されました: $newFilePath\n";
+fclose($fp);
+
+// 元ファイルを読み込む
+$newCsv = array_map('str_getcsv', file($newFilePath));
+
+echo "削除したグループのID: \n";
+echo implode("\n", $removedIds) . "\n";
+echo "処理前の行数: " . count($originalCsv) . "\n";
+echo "処理後の行数: " . count($newCsv) . "\n";
